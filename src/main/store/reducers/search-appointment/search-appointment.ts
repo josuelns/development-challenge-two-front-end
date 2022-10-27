a586dd02127d349ee6980fbea2dd5927db8c797f
josuelns <josuenavarro2000@hotmail.com>
2022-10-27 01:52:21 -0300

    chore: add appointment search

---
+++ src/main/store/reducers/search-appointment/search-appointment.ts
@@ -0,0 +1,56 @@
+import { AppointmentModel } from '@domain/models'
+import { SearchAppointment } from '@domain/usecases'
+import * as types from '@main/store/types';
+
+export interface SearchAppointmentState {
+  params: SearchAppointment.Params;
+  appointments: Array<AppointmentModel>;
+  loading: boolean;
+  error: string;
+}
+
+const initialState: SearchAppointmentState = {
+  params: {
+    title: "",
+    started_date: "",
+    ending_date: "",
+  },
+  appointments: [],
+  loading: false,
+  error: "",
+};
+
+export const SearchAppointmentReducer = (
+  state = initialState,
+  action: {
+    type: string;
+    payload: SearchAppointment.Model;
+  }
+) => {
+  switch (action.type) {
+    case types.SEARCH_APPOINTMENT_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        params: action.payload,
+        error: ''
+      };
+    case types.SEARCH_APPOINTMENT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: {
+          appointments: action.payload
+        },
+        error: ''
+      };
+    case types.SEARCH_APPOINTMENT_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+
+    default: return state
+  }
+};
