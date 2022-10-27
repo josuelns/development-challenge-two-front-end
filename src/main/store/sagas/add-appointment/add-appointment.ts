b7f1b346a6bccce388050f0175bb97256507ea0c
josuelns <josuenavarro2000@hotmail.com>
2022-10-27 01:52:37 -0300

    chore: add appointment add

---
+++ src/main/store/sagas/add-appointment/add-appointment.ts
@@ -0,0 +1,34 @@
+import { all, call, put, takeLatest } from '@redux-saga/core/effects'
+import { AddAppointment } from '@domain/usecases'
+import { addAppointmentFailure, addAppointmentSuccess, loadAppointmentRequest } from '@main/store/actions'
+import * as types from '@main/store/types'
+import {
+    makeRemoteAddAppointment
+} from '@main/factories/usecases'
+
+let response: AddAppointment.Model 
+
+const addAppointmentRequest = async (params: AddAppointment.Params) => {
+    const addAppointment = makeRemoteAddAppointment
+    try {
+        const data = await addAppointment.add({...params})
+        response = data
+        return data
+    }
+    catch (error) {
+        console.log(error)
+        return response
+    }
+}
+
+export function* addAppointmentData(action: { type: string, payload: AddAppointment.Params }) {
+    try {
+        yield call(addAppointmentRequest, action.payload)
+        yield put(addAppointmentSuccess(response))
+        yield put(loadAppointmentRequest())
+    } catch (error) {
+        yield put(addAppointmentFailure(response))
+    }
+}
+
+export const addAppointmentSaga = all([takeLatest(types.ADD_APPOINTMENT_REQUEST, addAppointmentData)])
