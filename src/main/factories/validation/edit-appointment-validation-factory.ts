5342947b3377f6e370770247503d777d180c1a80
josuelns <josuenavarro2000@hotmail.com>
2022-10-27 01:45:09 -0300

    chore: add appointment edit validation factory

---
+++ src/main/factories/validation/edit-appointment-validation-factory.ts
@@ -0,0 +1,8 @@
+import { ValidationComposite } from '@/main/composites'
+import { ValidationBuilder as Builder } from '@/main/builders'
+
+export const makeEditAppointmentValidation = (): ValidationComposite => ValidationComposite.build([
+  ...Builder.field('title').minLentgh(1).maxLentgh(60).required().build(),
+  ...Builder.field('started_date').date().minDate(new Date).notEqualDate('ending_date').required().build(),
+  ...Builder.field('ending_date').date().minDate(new Date).notEqualDate('started_date').biggerThen(('started_date')).required().build(),
+])
